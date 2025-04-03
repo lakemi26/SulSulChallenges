8d46e933233edde207626ce134ab38fbe60adecd
lakemi26 <lakemi@outlook.com.br>
2025-04-03 20:38:17 -0300

    Feat: Responsive Navbar

---
+++ components/navbar/navbar.js
@@ -0,0 +1,97 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+
+export const Navbar = () => {
+  const [hidden, setHidden] = useState(false);
+
+  function toggleSidebar() {
+    setHidden(!hidden);
+  }
+
+  return (
+    <nav className={styles.navbar}>
+      <ul
+        className={[`${styles.navList} ${styles.sidebar}`]}
+        style={{ display: hidden ? "none" : "flex" }}
+      >
+        <li onClick={toggleSidebar}>
+          <Link href={"/"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="26px"
+              viewBox="0 -960 960 960"
+              width="26px"
+              fill="#e3e3e3"
+            >
+              <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+            </svg>
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href={"/"} className={styles.navLink}>
+            Desafios
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href={"/"} className={styles.navLink}>
+            Randomizador
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href={"/"} className={styles.navLink}>
+            Dicas
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href={"/"} className={styles.navLink}>
+            Conta
+          </Link>
+        </li>
+      </ul>
+
+      <ul className={styles.navList}>
+        <li className={[`${styles.navItem}`]}>
+          <Link href={"/"}>
+            <Image src="/logo.png" alt="" height={45} width={130} />
+          </Link>
+        </li>
+        <li className={[`${styles.navItem} ${styles.hideOnMobile}`]}>
+          <Link href={"/"} className={styles.navLink}>
+            Desafios
+          </Link>
+        </li>
+        <li className={[`${styles.navItem} ${styles.hideOnMobile}`]}>
+          <Link href={"/"} className={styles.navLink}>
+            Randomizador
+          </Link>
+        </li>
+        <li className={[`${styles.navItem} ${styles.hideOnMobile}`]}>
+          <Link href={"/"} className={styles.navLink}>
+            Dicas
+          </Link>
+        </li>
+        <li className={[`${styles.navItem} ${styles.hideOnMobile}`]}>
+          <Link href={"/"} className={styles.navLink}>
+            Conta
+          </Link>
+        </li>
+        <li onClick={toggleSidebar} className={styles.menuButton}>
+          <Link href={"/"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="26px"
+              viewBox="0 -960 960 960"
+              width="26px"
+              fill="#e3e3e3"
+            >
+              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+            </svg>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
