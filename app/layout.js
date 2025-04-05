9b1bdae540b36bbe721a4731a50714a2005525ed
lakemi26 <lakemi@outlook.com.br>
2025-04-04 22:08:32 -0300

    Style: add new font-family (nunino)

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Nunito } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const nunito = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
 });
 
@@ -19,7 +24,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={`${nunito.variable} ${geistSans.variable} ${geistMono.variable} `}
+      >
         {children}
       </body>
     </html>
